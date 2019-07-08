65fc8e6809096376c2ebe343118ece6fea048f4b
Luca Gasperini <info@xsoftware.it>
2019-07-08 13:03:19 +0200

    Added internationalization
    Signed-off-by: Luca Gasperini <info@xsoftware.it>

--- xsoftware-products-ecommerce.php
+++ xsoftware-products-ecommerce.php
@@ -1,18 +1,18 @@
 <?php
 /*
-Plugin Name: XSoftware Products E-Commerce
-Description: Products E-Commerce management on wordpress.
+Plugin Name: XSoftware Template E-Commerce
+Description: XSoftware Template for xsoftware.it website.
 Version: 1.0
 Author: Luca Gasperini
 Author URI: https://xsoftware.it/
-Text Domain: xsoftware_products
+Text Domain: xs_tmp
 */
 
 if(!defined("ABSPATH")) die;
 
-if (!class_exists("xs_products_ecommerce_plugin")) :
+if (!class_exists("xs_template_html_plugin")) :
 
-class xs_products_ecommerce_plugin
+class xs_template_html_plugin
 {
 
         private $options = array( );
@@ -38,7 +38,14 @@ class xs_products_ecommerce_plugin
                 add_filter('xs_product_single_html', [ $this, 'single_html' ], 0, 2);
                 /* Use @xs_framework_menu_items to print cart menu item */
                 add_filter('xs_framework_menu_items', [ $this, 'cart_menu_item' ], 2);
+                add_action( 'plugins_loaded', [ $this, 'l10n_load' ] );
         }
+
+        function l10n_load()
+        {
+                load_plugin_textdomain('xs_tmp', false, basename( dirname( __FILE__ ) ).'/l10n/');
+        }
+
         /*
         *  array : cart_menu_item : array
         *  This method is used to create the menu items
@@ -64,7 +71,8 @@ class xs_products_ecommerce_plugin
 
                 /* Add a child menu item for shopping cart */
                 $items[] = xs_framework::insert_nav_menu_item([
-                        'title' => '<i class="fas fa-shopping-cart"></i><span>Carrello</span>',
+                        'title' => '<i class="fas fa-shopping-cart"></i>
+                                <span>'.__('Cart','xs_tmp').'</span>',
                         'url' => $this->checkout,
                         'order' => 101,
                         'parent' => $top->ID
@@ -72,21 +80,25 @@ class xs_products_ecommerce_plugin
 
                 /* If user is logged print Logout item, else Login item*/
                 if(is_user_logged_in()) {
+
                         $items[] = xs_framework::insert_nav_menu_item([
-                                'title' => '<i class="fas fa-file-invoice"></i>Fatture</span>',
+                                'title' => '<i class="fas fa-file-invoice"></i>
+                                        <span>'.__('Invoices','xs_tmp').'</span>',
                                 'url' => $this->checkout.'?invoice',
                                 'order' => 102,
                                 'parent' => $top->ID
                         ]);
                         $items[] = xs_framework::insert_nav_menu_item([
-                                'title' => '<i class="fas fa-sign-out-alt"></i>Logout</span>',
+                                'title' => '<i class="fas fa-sign-out-alt"></i>
+                                        <span>'.__('Logout','xs_tmp').'</span>',
                                 'url' => wp_logout_url( home_url() ),
                                 'order' => 103,
                                 'parent' => $top->ID
                         ]);
                 } else {
                         $items[] = xs_framework::insert_nav_menu_item([
-                                'title' => '<i class="fas fa-sign-in-alt"></i><span>Login</span>',
+                                'title' => '<i class="fas fa-sign-in-alt"></i>
+                                        <span>'.__('Login','xs_tmp').'</span>',
                                 'url' => wp_login_url( home_url() ),
                                 'order' => 102,
                                 'parent' => $top->ID
@@ -112,14 +124,15 @@ class xs_products_ecommerce_plugin
                         'xs_product_template',
                         plugins_url('style/single.min.css',__FILE__)
                 );
+
                 /* Create a button with $post_id as GET value*/
                 $btn = xs_framework::create_button([
                         'name' => 'add_cart',
                         'value' => $post_id,
-                        'text' => 'Aggiungi al Carrello'
+                        'text' => __('Add to Cart','xs_tmp')
                 ]);
                 /* Create a number input as quantity of this item */
-                $qt_label = '<span>Mesi:</span>';
+                $qt_label = '<span>'.__('Months','xs_tmp').':</span>';
                 $qt = xs_framework::create_input_number([
                         'name' => 'qt',
                         'value' => 1,
@@ -167,7 +180,7 @@ class xs_products_ecommerce_plugin
                 echo '</div>';
                 if(!empty($price)) {
                         echo '<div class="cart">';
-                        echo '<span>Prezzo:</span>';
+                        echo '<span>'.__('Price','xs_tmp').':</span>';
                         echo '<i>'.$price['price'].' '.$price['currency_symbol'].'</i>';
                         echo apply_filters('xs_cart_add_html', $id);
                         echo '</div>';
@@ -203,7 +216,7 @@ class xs_products_ecommerce_plugin
                         $output .= '</div>';
                         if(!empty($price)) {
                                 $output .= '<div class="price">';
-                                $output .= '<p>Al prezzo mensile di:</p>';
+                                $output .= '<p>'.__('Price per month','xs_tmp').':</p>';
                                 $output .= '<i>'.$price['price'].
                                 ' '.$price['currency_symbol'].'</i>';
                                 $output .= '</div>';
@@ -213,6 +226,7 @@ class xs_products_ecommerce_plugin
                 $output .= '</div>';
                 return $output;
         }
+
         /*
         *  string : show_cart_html : array
         *  This method is used to create the html page for no empty cart
@@ -220,12 +234,15 @@ class xs_products_ecommerce_plugin
         function show_cart_html($so)
         {
                 /* Add the css style */
-                wp_enqueue_style('xs_cart_checkout_style', plugins_url('style/cart.css', __FILE__));
-
+                wp_enqueue_style(
+                        'xs_cart_checkout_style',
+                        plugins_url('style/cart.css', __FILE__)
+                );
                 /* Print the HTML */
                 $output = '';
                 /* Create table array */
                 $table = array();
+
                 /* Get the currency symbol */
                 $symbol = $so['transaction']['currency_symbol'];
 
@@ -238,7 +255,8 @@ class xs_products_ecommerce_plugin
                         $tmp[] = $item['discount'];
                         $tmp[] = $item['tax_code'];
                         $tmp[] = $item['subtotal'] . ' ' . $symbol;
-                        $tmp[] = '<a href="?rem_cart='.$item['id'].'">Rimuovi</a>';
+                        $tmp[] = '<a href="?rem_cart='.$item['id'].'">'.
+                                __('Remove','xs_tmp').'</a>';
 
                         $display_items[] = $tmp;
                 }
@@ -247,22 +265,23 @@ class xs_products_ecommerce_plugin
                         'class' => 'items',
                         'data' => $display_items,
                         'headers' => [
-                                'Descrizione',
-                                'Quantità',
-                                'Prezzo unitario',
-                                'Sconto (%)',
-                                'IVA',
-                                'Subtotale',
-                                'Azioni'
+                                __('Description','xs_tmp'),
+                                __('Quantity','xs_tmp'),
+                                __('Price','xs_tmp'),
+                                __('Discount (%)','xs_tmp'),
+                                __('VAT','xs_tmp'),
+                                __('Subtotal','xs_tmp'),
+                                __('Actions','xs_tmp')
                         ],
                         'echo' => FALSE
                 ]);
+
                 /* Get the global property from sale order */
-                $t['subtotal'][0] = '<strong>Imponibile:</strong>';
+                $t['subtotal'][0] = '<strong>'.__('Subtotal','xs_tmp').':</strong>';
                 $t['subtotal'][1] = $so['transaction']['subtotal'] . ' ' . $symbol;
-                $t['taxed'][0] = '<strong>IVA:</strong>';
+                $t['taxed'][0] = '<strong>'.__('VAT','xs_tmp').':</strong>';
                 $t['taxed'][1] = $so['transaction']['tax'] . ' ' . $symbol;
-                $t['total'][0] = '<strong>Totale:</strong>';
+                $t['total'][0] = '<strong>'.__('Total','xs_tmp').':</strong>';
                 $t['total'][1] = $so['transaction']['total'] . ' ' . $symbol;
                 /* Get the table */
                 $output .= xs_framework::create_table([
@@ -270,16 +289,17 @@ class xs_products_ecommerce_plugin
                         'data' => $t,
                         'echo' => FALSE
                 ]);
+
                 /* Get the form for discount code */
                 $output .= '<form action="" method="GET">';
                 /* Print discount code label and text input */
-                $label = '<span>Codice Sconto:</span>';
+                $label = '<span>'.__('Discount Code','xs_tmp').':</span>';
                 $discount = xs_framework::create_input([
                         'name' => 'discount'
                 ]);
                 /* Print the button */
                 $button = xs_framework::create_button([
-                        'text' => 'Applica lo sconto'
+                        'text' => __('Apply discount','xs_tmp')
                 ]);
                 /* Print the container */
                 $output .= xs_framework::create_container([
@@ -305,7 +325,7 @@ class xs_products_ecommerce_plugin
                 );
                 /* Print the HTML */
                 $output = '';
-                $output .= '<h2>Il carrello è vuoto!</h2>';
+                $output .= '<h2>'.__('The cart is empty!','xs_tmp').'</h2>';
                 /* Return the HTML */
                 return $output;
         }
@@ -320,9 +340,10 @@ class xs_products_ecommerce_plugin
                         'xs_cart_checkout_style',
                         plugins_url('style/cart.min.css', __FILE__)
                 );
+
                 /* Print the HTML */
                 $output = '';
-                $output .= '<h2>Il pagamento è stato eseguito con successo!</h2>';
+                $output .= '<h2>'.__('The payment was successful!','xs_tmp').'</h2>';
                 /* Print the invoice pdf on a frame */
                 $output .= '<iframe src="data:application/pdf;base64,'.$info['pdf']['base64'].'"
                         class="xs_cart_pdf_frame"></iframe>';
@@ -345,7 +366,7 @@ class xs_products_ecommerce_plugin
                         $tmp[] = $i['transaction']['total'] . ' ' . $symbol;
                         $tmp[] = xs_framework::create_link([
                                 'href' => $this->checkout.'?invoice='.$i['invoice']['id'],
-                                'text' => 'Apri',
+                                'text' => __('Open','xs_tmp'),
                                 'echo' => FALSE
                         ]);
 
@@ -357,12 +378,12 @@ class xs_products_ecommerce_plugin
                         'class' => 'invoice_list',
                         'data' => $display,
                         'headers' => [
-                                'ID',
-                                'Nome',
-                                'Data',
-                                'Intestatario',
-                                'Importo',
-                                'Azioni',
+                                __('ID','xs_tmp'),
+                                __('Name','xs_tmp'),
+                                __('Date','xs_tmp'),
+                                __('Accountholder','xs_tmp'),
+                                __('Amount','xs_tmp'),
+                                __('Actions','xs_tmp'),
                         ],
                         'echo' => FALSE
                 ]);
@@ -373,9 +394,11 @@ class xs_products_ecommerce_plugin
         {
                 if(!is_array($info)) {
                         if($info === 0)
-                                return '<h1>La fattura selezionata non esiste!</h1>';
+                                return '<h1>'.__(
+                                        'The selected invoice does not exist!','xs_tmp').'</h1>';
                         if($info === 1)
-                                return '<h1>Non hai i permessi per entrare qui!</h1>';
+                                return '<h1>'.__('You do not have permission to log in here!',
+                                        'xs_tmp').'</h1>';
                 }
                 /* Print the HTML */
                 $output = '';
@@ -390,6 +413,6 @@ class xs_products_ecommerce_plugin
 
 endif;
 
-$xs_products_ecommerce_plugin = new xs_products_ecommerce_plugin();
+$xs_template_html_plugin = new xs_template_html_plugin();
 
 ?>
